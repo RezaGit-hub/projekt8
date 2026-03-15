c963aeec44c0e9414f0db5fd34ec4ffb2dae63b0
Reza Adamir <stu238357@mail.uni-kiel.de>
2026-03-15 18:59:54 +0100

    main.py updated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.database import get_connection
-from app.init_db import create_table
+
 from app.api.patients import router as patient_router
 from app.api.doctors import router as doctor_router
 from app.api.appointments import router as appointment_router
